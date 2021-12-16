7ef1c724770d804160e977aba581eeac4402f72d
MrJanHorak <janny_b_goode@hotmail.com>
2021-12-15 21:00:32 -0500

    create Post model

--- models/group.js
+++ models/group.js
@@ -1,40 +1,42 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const groupSchema = new Schema({
-  title: {type: String, required: true, unique: true},
-  category: String,
-  avatar: {type: String, required: true},
-  members: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Profile'
-    }
-  ],
-  owner: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Profile'
-    }
-  ],
-  posts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Post'
-    }
-  ],
-  admin: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Profile'
-    }
-  ],
-  
-},{
+const groupSchema = new Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    category: String,
+    avatar: { type: String, required: true },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Profile",
+      },
+    ],
+    owner: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Profile",
+      },
+    ],
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    admin: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Profile",
+      },
+    ],
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
-const Group = mongoose.model('Group', groupSchema)
+const Group = mongoose.model("Group", groupSchema);
 
-export {Group}
+export { Group };
