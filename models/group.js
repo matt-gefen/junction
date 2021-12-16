947df7e9e3feb5930fcafa27ecf5aef5398dbe38
MrJanHorak <janny_b_goode@hotmail.com>
2021-12-15 20:53:28 -0500

    create Group model

---
+++ models/group.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const groupSchema = new Schema({
+  title: {type: String, required: true, unique: true},
+  category: String,
+  avatar: {type: String, required: true},
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Profile'
+    }
+  ],
+  owner: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Profile'
+    }
+  ],
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post'
+    }
+  ],
+  admin: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Profile'
+    }
+  ],
+  
+},{
+    timestamps: true,
+})
+
+const Group = mongoose.model('Group', groupSchema)
+
+export {Group}
