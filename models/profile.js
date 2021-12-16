4d0b1976d240758c7f72101647508e003a60e496
MrJanHorak <janny_b_goode@hotmail.com>
2021-12-15 20:45:35 -0500

    edit Profile model

--- models/profile.js
+++ models/profile.js
@@ -3,6 +3,42 @@ import mongoose from 'mongoose'
 const profileSchema = new mongoose.Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
+  first_name: {type: String, required: true},
+  last_name: {type: String, required: true},
+  avatar: {type: String, required: true},
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post'
+    }
+  ],
+  groups: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Group'
+    }
+  ],
+  joined_groups: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Group'
+    }
+  ],
+  favorited_posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post'
+    }
+  ],
+  category_prefs: String,
+  location: String,
+  registered_events: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post'
+    }
+  ]
+
 },{
     timestamps: true,
 })
