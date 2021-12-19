7ed55850d4be85aef2ed0173c4acd2f49933f15a
ChrisJeffreys <ChrisBJeffreys@gmail.com>
2021-12-19 15:04:17 -0600

    add New Comment component in Post Details

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Login from './pages/Login/Login'
 import CreateGroup from './pages/CreateGroup/CreateGroup'
 import Group from './pages/Group/Group'
 import CreatePost from './pages/CreatePost/CreatePost'
-import PostDetails from './pages/PostDetails/PostDetails'
+import PostDetails from './pages/Post/PostDetails'
 import * as authService from './services/authService'
 import GroupIndex from './pages/GroupIndex/GroupIndex'
 
@@ -56,7 +56,7 @@ const App = () => {
         />
         <Route 
           path="/groups/:id/posts/:postId"
-          element={<PostDetails />}
+          element={<PostDetails user={user}/>}
         />
       </Routes>
     </>
