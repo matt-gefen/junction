16ed97c7af0b5b78d25a54369da23d4be9f79143
MrJanHorak <janny_b_goode@hotmail.com>
2021-12-20 11:24:45 -0500

    add handleAddCategory to categoryMenu

--- src/components/CategoryNav/CategoryMenu.jsx
+++ src/components/CategoryNav/CategoryMenu.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import categories from '../../data/categories'
 import styles from './CategoryNav.module.css'
+import updateProfile from '../../services/profileService'
 
 const CategoryMenu = (props) => {
-
-let groupCategories = new Set(categories.map((element, index) => {
-  return element
-}))
-
-let categoryOptions = []
-
+  
+  let groupCategories = new Set(categories.map((element, index) => {
+    return element
+  }))
+  
+  let categoryOptions = []
+  
+  const handleAddCategory = async (categoryId) => {
+    try {
+      const updatedProfile = await updateProfile(categoryId)
+      setProfile(categories.map((category) => (category._id === categoryId ? updatedProfile : category)))
+    } catch (error) {
+      throw error
+    }
+  }
 groupCategories.forEach((element, index) =>{
   categoryOptions.push(
-   <div className={styles.categoryName} key={index}>{element}</div> 
+   <div onClick={() => props.handleAddCategory(props.category._id)} className={styles.categoryName} key={index}>{element}</div> 
   )
 })
+
 
   return (
 
