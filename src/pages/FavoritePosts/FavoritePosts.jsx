07cd08209fbb77b0e9896f8f908d30ae9ac9723f
mattgefen <matt.gefen@gmail.com>
2021-12-20 21:54:13 -0600

    stubbing favPosts

---
+++ src/pages/FavoritePosts/FavoritePosts.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import PostCard from "../../components/PostCard/PostCard";
+
+const FavoritePosts = (props) => {
+
+
+  return (
+    <div className="layout">
+    <div className="group-details">
+        {/* <section className={styles.container}>
+          {group.posts?.map(post => (
+            <PostCard user={props.user} group={group} post={post} profile={profile}/>
+          ))}
+        </section> */}
+  
+  </div>
+</div>
+);
+
+
+}
+
+export default FavoritePosts;
