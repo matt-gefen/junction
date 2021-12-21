65fa13551040e3926f1e8cfc0ae0ee256a42ba93
mattgefen <matt.gefen@gmail.com>
2021-12-21 17:50:45 -0600

    functional search, not saving marker yet

--- src/components/LocationSearch/LocationSearch.jsx
+++ src/components/LocationSearch/LocationSearch.jsx
@@ -1,54 +1,74 @@
-import React, { useRef, useEffect, useState } from 'react'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import React, { useState, useRef, useCallback } from 'react'
+import MapGL, {GeolocateControl} from 'react-map-gl'
+import Geocoder from 'react-map-gl-geocoder'
+import MAP_STYLE from './MapStyle.json';
 
-import styles from './LocationSearch.module.css'
+// Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
+const MAPBOX_TOKEN = 'pk.eyJ1IjoibWdlZmVuIiwiYSI6ImNreGduZzBidjBoZzQydm1sNzducGJ2azgifQ.inJ-UIK78Vd7VGc91RiRug'
 
-import mapboxgl from 'mapbox-gl'
-mapboxgl.accessToken = 'pk.eyJ1IjoibWdlZmVuIiwiYSI6ImNreGduZzBidjBoZzQydm1sNzducGJ2azgifQ.inJ-UIK78Vd7VGc91RiRug'
+const geolocateStyle = {
+  top: 0,
+  left: 0,
+  margin: 10
+};
+const positionOptions = {enableHighAccuracy: true};
 
-const LocationSearch = (props) => {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-98.56);
-  const [lat, setLat] = useState(39.66);
-  const [zoom, setZoom] = useState(3);
+const LocationSearch = () => {
+  const [viewport, setViewport] = useState({
+    latitude: 37.8,
+    longitude: 96,
+    zoom: 3,
+    bearing: 0,
+    pitch: 0
+  });
+  const mapRef = useRef();
+  const handleViewportChange = useCallback(
+    (newViewport) => setViewport(newViewport),
+    []
+  );
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    if ("geolocation" in navigator) { 
-      navigator.geolocation.getCurrentPosition(position => { 
-          map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [position.coords.longitude, position.coords.latitude],
-            zoom: 8
-          });
-      }); 
-  } else {
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
-    });
-  } 
-  // const geocoder = new mapboxgl.MapboxGeocoder({
-  //   // Initialize the geocoder
-  //   accessToken: mapboxgl.accessToken, // Set the access token
-  //   mapboxgl: mapboxgl, // Set the mapbox-gl instance
-  //   marker: false // Do not use the default marker style
-  // });
-  
-  // // Add the geocoder to the map
-  // map.addControl(geocoder);
-  
-})
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  const handleGeocoderViewportChange = useCallback(
+    (newViewport) => {
+      const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+      return handleViewportChange({
+        ...newViewport,
+        ...geocoderDefaultOverrides
+      });
+    },
+    []
+  );
 
   return (
-    <div>
-    <div ref={mapContainer} className={styles.mapContainer} />
+    <div style={{ height: "100vh" }}>
+      <MapGL
+        ref={mapRef}
+        {...viewport}
+        width="50%"
+        height="50%"
+        onViewportChange={handleViewportChange}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+        mapStyle={MAP_STYLE}
+      >
+      <GeolocateControl
+        style={geolocateStyle}
+        positionOptions={positionOptions}
+        trackUserLocation
+        showUserLocation={false}
+        auto
+      />
+        <Geocoder
+          mapRef={mapRef}
+          onViewportChange={handleGeocoderViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          position="top-left"
+        />
+      </MapGL>
     </div>
-    );
-
-}
+  );
+};
 
 export default LocationSearch
