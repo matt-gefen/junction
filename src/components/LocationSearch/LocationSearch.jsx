08bfeda740fc002ccb913ccc3b40526f4caa9982
mattgefen <matt.gefen@gmail.com>
2021-12-21 21:26:27 -0600

    Location added to Group Create!

--- src/components/LocationSearch/LocationSearch.jsx
+++ src/components/LocationSearch/LocationSearch.jsx
@@ -1,9 +1,10 @@
 import 'mapbox-gl/dist/mapbox-gl.css'
 import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import React, { useState, useRef, useCallback } from 'react'
-import MapGL, {GeolocateControl} from 'react-map-gl'
+import MapGL, {GeolocateControl, Marker} from 'react-map-gl'
 import Geocoder from 'react-map-gl-geocoder'
 import MAP_STYLE from './MapStyle.json';
+import styles from './LocationSearch.module.css'
 
 // Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
 const MAPBOX_TOKEN = 'pk.eyJ1IjoibWdlZmVuIiwiYSI6ImNreGduZzBidjBoZzQydm1sNzducGJ2azgifQ.inJ-UIK78Vd7VGc91RiRug'
@@ -15,7 +16,9 @@ const geolocateStyle = {
 };
 const positionOptions = {enableHighAccuracy: true};
 
-const LocationSearch = () => {
+const LocationSearch = (props) => {
+  // const [searchResult, setSearchResult] = useState()
+  const [resultLatLong, setResultLatLong] = useState()
   const [viewport, setViewport] = useState({
     latitude: 37.8,
     longitude: 96,
@@ -42,8 +45,14 @@ const LocationSearch = () => {
     []
   );
 
+  const onResult = (results) => {
+    console.log(results.result.place_name)
+    props.setLocation(results.result.place_name)
+    setResultLatLong(results.result.geometry.coordinates)
+  }
+
   return (
-    <div style={{ height: "100vh" }}>
+    <div className={styles.mapContainer}>
       <MapGL
         ref={mapRef}
         {...viewport}
@@ -53,20 +62,29 @@ const LocationSearch = () => {
         mapboxApiAccessToken={MAPBOX_TOKEN}
         mapStyle={MAP_STYLE}
       >
-      <GeolocateControl
-        style={geolocateStyle}
-        positionOptions={positionOptions}
-        trackUserLocation
-        showUserLocation={false}
-        auto
-      />
-      <Geocoder
-        mapRef={mapRef}
-        proximity={{longitude: viewport.longitude, latitude: viewport.latitude}}
-        onViewportChange={handleGeocoderViewportChange}
-        mapboxApiAccessToken={MAPBOX_TOKEN}
-        position="top-left"
+        <GeolocateControl
+          style={geolocateStyle}
+          positionOptions={positionOptions}
+          trackUserLocation
+          showUserLocation={false}
+          auto
         />
+        <Geocoder
+          mapRef={mapRef}
+          proximity={{longitude: viewport.longitude, latitude: viewport.latitude}}
+          onViewportChange={handleGeocoderViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          position="top-left"
+          width="100%"
+          onResult={onResult}
+          marker={true}
+        />
+        <Marker
+          latitude={viewport.latitude}
+          longitude={viewport.longitude}
+        >
+          <p style={{fontSize:"20px"}}>📌</p>
+        </Marker>
       </MapGL>
     </div>
   );
