925fb163adf74e0b0b8fc782a75d2d583652eeb4
ChrisJeffreys <ChrisBJeffreys@gmail.com>
2021-12-22 14:08:41 -0600

    add AppNavBar for non users

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,22 +5,10 @@ import AppNavBar from '../../components/MaterialUI/AppNavBar'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
-    <>
-      {user ?
-        <AppNavBar 
-          user={user}
-          handleLogout={handleLogout}
-        />
-      :
-        <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-            <li><Link to="/groups">Discover Groups</Link></li>
-          </ul>
-        </nav>
-      }
-    </>
+    <AppNavBar 
+      user={user}
+      handleLogout={handleLogout}
+    />
   )
 }
 
