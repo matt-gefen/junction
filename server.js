7e037ba778d1e6ef8b05f678038e5b8b78f6dbd1
ChrisJeffreys <ChrisBJeffreys@gmail.com>
2021-12-23 09:33:50 -0600

    coupled server fix

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/api/groups', groupRouter)
 
 app.get('/*', function (req, res) {
   res.sendFile(
-    path.dirname(fileURLToPath(import.meta.url), 'build', 'index.html')
+    path.join(path.dirname(fileURLToPath(import.meta.url)), 'build', 'index.html')
   )
 })
 
