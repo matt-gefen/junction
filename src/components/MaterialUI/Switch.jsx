e5c29246a82092a7bd599b991a4b37b8761223a3
MrJanHorak <janny_b_goode@hotmail.com>
2021-12-22 21:16:41 -0500

    update event registration on post create

--- src/components/MaterialUI/Switch.jsx
+++ src/components/MaterialUI/Switch.jsx
@@ -1,15 +1,29 @@
 import * as React from 'react';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 
-const label = { inputProps: { 'aria-label': 'Switch demo' } };
+export default function SwitchLabels(props) {
 
-export default function BasicSwitches() {
+  const handleSwitch = (event) => {
+    console.log(event.target.value)
+    let position=event.target.value
+
+    if (!position){
+    props.setHasRegistration(true)
+    event.target.value=true
+    event.target.name="registration"
+    props.onChange(event)
+    } else {
+      props.setHasRegistration(false)
+      event.target.value=false
+      event.target.name="registration"
+      props.onChange(event)
+    }
+  }
   return (
-    <div>
-      <Switch {...label} defaultChecked />
-      <Switch {...label} />
-      <Switch {...label} disabled defaultChecked />
-      <Switch {...label} disabled />
-    </div>
+    <FormGroup>
+      <FormControlLabel control={<Switch checked={props.hasRegistration} onChange={handleSwitch} value={props.hasRegistration} name="registration" />} label="Registration for Event?" />
+    </FormGroup>
   );
 }
